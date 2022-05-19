e0a3ff920524a15f441f55fcaeab2e496b2d523e
Ola <kozlowska.aleksandra92@gmail.com>
2022-05-19 23:25:28 +0200

    Start

---
+++ src/test/java/pl/akoz/zadanie26/Zadanie26ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.akoz.zadanie26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie26ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
